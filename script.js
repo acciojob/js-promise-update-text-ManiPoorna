d797e9b6b5601ebc83b0869a9a64f31df7fd7519
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-06-23 23:37:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function prom(){
     return new Promise((resolve)=>{
         setTimeout(()=>{
-            resolve("Hello World!");
+            resolve("Hello, world!");
         },1000)
     })
 }
 let x = prom();
-x.then((data)=>{
-    const div = document.getElementById("output");
+x.then(data=>{
+	const div = document.getElementById("output");
     div.innerText = data;
-    console.log(data);
 })
