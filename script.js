abfba5ec0e991f6a543867825f0526961cfcd5ea
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-06-23 23:30:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+function prom(){
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve("Hello World!")
+		},1000)
+	})
+}
+
+let x = prom();
+x.then((data)=>{
+	const div = document.getEelentById("output");
+	div.innerText = data;
+})
